ad4bdeb0e5e24858d5476627f9da13d5639ebf65
Pratyush7218 <pratyushchoudhary72@gmail.com>
2025-02-21 16:15:53 +0530

    Pratyush Choudhary Portfolio

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, url_for, flash
+
+
+app = Flask(__name__)  # Initialize Flask app
+
+@app.route('/')  # Define the route for the homepage
+def hello():
+    return render_template('index.html')  # Return response
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Run the app in debug mode
